159b8ccb13772572149f14879a87b9314dad5b0b
Ray Tsou <rtsou@logicpallet.com>
2025-12-02 00:14:10 -0500

    day 1 part 2

--- day_1/sol.py
+++ day_1/sol.py
@@ -9,12 +9,19 @@ class Dial:
     def __init__(self):
         self.__val = 50
 
-    def rotate(self, direction: str, num: int) -> None:
+    def rotate(self, direction: str, distance: int) -> None:
         match(direction):
             case 'L':
-                self.__val = (self.__val - num) % self.RANGE
+                self.__val = (self.__val - distance) % self.RANGE
             case 'R':
-                self.__val = (self.__val + num) % self.RANGE
+                self.__val = (self.__val + distance) % self.RANGE
+
+    def get_zero_crossover_count(self, direction: str, distance: int) -> int:
+        match(direction):
+            case 'L':
+                return (distance - self.__val + 99) // self.RANGE - (self.__val == 0)
+            case 'R':
+                return (distance + self.__val - 1) // self.RANGE
 
     @property
     def value(self) -> int:
@@ -24,16 +31,21 @@ class Dial:
 def main():
     dial = Dial()
     zero_cnt = 0
+    zero_crosses = 0
     with open(INPUT_FILE, mode='r') as f:
         for rotation in f:
             direction = rotation[0]
             distance = int(rotation[1:])
+
+            x = dial.get_zero_crossover_count(direction, distance)
+            zero_crosses += x
             dial.rotate(direction, distance)
 
             if (dial.value == 0):
                 zero_cnt += 1
 
-    print(zero_cnt)
+    print(f"Part I: {zero_cnt}")
+    print(f"Part II: {zero_cnt + zero_crosses}")
 
 
 if __name__ == '__main__':
