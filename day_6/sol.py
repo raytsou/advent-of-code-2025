676a27ed84454f9ef161b0b42e59f15bf64fa821
Ray Tsou <rtsou@logicpallet.com>
2025-12-08 22:23:13 -0500

    day 6 part 2

--- day_6/sol.py
+++ day_6/sol.py
@@ -1,28 +1,38 @@
 #!/usr/bin/env python3
 import re
 import numpy as np
-# from typing import List, Tuple
+from typing import List, Sequence
 
 INPUT_FILE = 'input.txt'
+
+
+def calc(nums: Sequence[Sequence[int]], ops: List[str]) -> int:
+    tot = 0
+    for row, op in zip(nums, ops):
+        if op == '+':
+            tot += sum(row)
+        else:
+            tot += np.prod(row)
+
+    return tot
 
 
 def main():
     with open(INPUT_FILE, mode='r') as f:
         txt = f.read().rstrip().split('\n')
-        nums = txt[:-1]
-        ops = txt[-1]
 
-        nums = np.array([list(map(int, filter(len, re.split(r"\s+", x)))) for x in nums]).T
-        ops = re.split(r'\s+', ops)
+        nums = np.array([list(map(int, filter(len, re.split(r"\s+", x)))) for x in txt[:-1]]).T
+        ops = re.split(r'\s+', txt[-1])
 
-        tot = 0
-        for row, op in zip(nums, ops):
-            if op == '+':
-                tot += sum(row)
-            else:
-                tot += np.prod(row)
+        print(f"Part I: {calc(nums, ops)}")
 
-        print(f"Part I: {tot}")
+        nums = [reversed(x) for x in txt[:-1]]
+        nums = list(map(list, zip(*nums)))
+        nums = '\n'.join([''.join(x) for x in nums])
+        nums = [x.split('\n') for x in re.split(r"\n\s+\n", nums)]
+        nums = [list(map(int, filter(len, x))) for x in nums]
+
+        print(f"Part II: {calc(nums, reversed(ops))}")
 
 
 if __name__ == '__main__':
