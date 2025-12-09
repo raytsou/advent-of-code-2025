8f3ddb0b7821dea2fabf99539dbf42b49bf19f9c
Ray Tsou <rtsou@logicpallet.com>
2025-12-08 21:03:25 -0500

    day 5 part 2

--- day_5/sol.py
+++ day_5/sol.py
@@ -15,6 +15,20 @@ def count_fresh(ranges: List[Tuple[int]], ingredients: List[int]) -> int:
     return fresh_cnt
 
 
+def count_size(ranges: List[Tuple[int]]) -> int:
+    ranges.sort()
+    size = 0
+    prev_upper = 0
+    for lower, upper in ranges:
+        if lower <= prev_upper:
+            size += max(upper - prev_upper, 0)
+        else:
+            size += upper - lower + 1
+        prev_upper = upper
+
+    return size
+
+
 def main():
     with open(INPUT_FILE, mode='r') as f:
         ranges, ingredients = [x.rstrip().split('\n') for x in f.read().split("\n\n")]
@@ -22,6 +36,7 @@ def main():
         ingredients = [int(x) for x in ingredients]
 
         print(f"Part I: {count_fresh(ranges, ingredients)}")
+        print(f"Part II: {count_size(ranges)}")
 
 
 if __name__ == '__main__':
