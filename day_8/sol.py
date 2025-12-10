e2ff980488ee0b00f8e79f2c8d54058391b8f3c2
Ray Tsou <rtsou@logicpallet.com>
2025-12-09 23:19:54 -0500

    day 8 part 2

--- day_8/sol.py
+++ day_8/sol.py
@@ -3,7 +3,6 @@ from dataclasses import dataclass
 from typing import (
     List,
     Self,
-    Set,
 )
 
 INPUT_FILE = 'input.txt'
@@ -29,57 +28,71 @@ class Point:
         ) ** 0.5
 
 
-def connect(points: List[Point], connections: int) -> List[Set[Point]]:
-    dists = dict()
-    for i, pt1 in enumerate(points):
-        for pt2 in points[i+1:]:
-            pair = (pt1, pt2) if pt1 < pt2 else (pt2, pt1)
-            dists[pair] = pt1.distance_to(pt2)
+class JunctionMap:
+    def __init__(self, points: List[Point]):
+        self.__points = points
+        self.__dists = dict()
+        for i, pt1 in enumerate(points):
+            for pt2 in points[i+1:]:
+                pair = (pt1, pt2) if pt1 < pt2 else (pt2, pt1)
+                self.__dists[pair] = pt1.distance_to(pt2)
 
-    closest_pts = sorted(dists, key=dists.get)[:connections]
+    def connect(self, connections: int) -> int:
+        if connections < 0:
+            connections = len(self.__dists)
 
-    cliques = dict()
-    cidx = 0
-    clique_map = {p: None for p in points}
-    for pt1, pt2 in closest_pts:
-        c1_id = clique_map[pt1]
-        c2_id = clique_map[pt2]
+        cliques = dict()
+        cidx = 0
+        clique_map = {p: None for p in self.__points}
+        closest_dists = sorted(self.__dists, key=self.__dists.get)[:connections]
+        for pt1, pt2 in closest_dists:
+            c1_id = clique_map[pt1]
+            c2_id = clique_map[pt2]
 
-        if c1_id is not None:
-            if c2_id is not None:
-                if c1_id == c2_id:
-                    continue
+            if c1_id is not None:
+                if c2_id is not None:
+                    if c1_id == c2_id:
+                        continue
 
-                c2 = cliques[c2_id]
-                for pt in c2:
-                    clique_map[pt] = c1_id
+                    c2 = cliques[c2_id]
+                    for pt in c2:
+                        clique_map[pt] = c1_id
 
-                cliques[c1_id] = cliques[c1_id].union(c2)
-                cliques.pop(c2_id)
+                    cliques[c1_id] = cliques[c1_id].union(c2)
+                    cliques.pop(c2_id)
+                else:
+                    cliques[c1_id].add(pt2)
+                    clique_map[pt2] = c1_id
             else:
-                cliques[c1_id].add(pt2)
-                clique_map[pt2] = c1_id
-        else:
-            if c2_id is not None:
-                cliques[c2_id].add(pt1)
-                clique_map[pt1] = c2_id
-            else:
-                cliques[cidx] = {pt1, pt2}
-                clique_map[pt1] = cidx
-                clique_map[pt2] = cidx
-                cidx += 1
-    ret = 1
-    for i in range(3):
-        idx = max(cliques, key=lambda x: len(cliques.get(x)))
-        ret *= len(cliques[idx])
-        cliques.pop(idx)
-    return ret
+                if c2_id is not None:
+                    cliques[c2_id].add(pt1)
+                    clique_map[pt1] = c2_id
+                else:
+                    cliques[cidx] = {pt1, pt2}
+                    clique_map[pt1] = cidx
+                    clique_map[pt2] = cidx
+                    cidx += 1
+
+            if (
+                len(cliques) == 1 and
+                None not in clique_map.values()
+            ):
+                return pt1.x * pt2.x
+
+        ret = 1
+        for i in range(3):
+            idx = max(cliques, key=lambda x: len(cliques.get(x)))
+            ret *= len(cliques[idx])
+            cliques.pop(idx)
+        return ret
 
 
 def main():
     with open(INPUT_FILE, mode='r') as f:
         pts = [Point(*map(int, x.split(','))) for x in f.read().split('\n')]
-        print(f"Part I: {connect(pts, 1000)}")
+        m = JunctionMap(pts)
+        print(f"Part I: {m.connect(1000)}")
+        print(f"Part II: {m.connect(-1)}")
 
 
 if __name__ == '__main__':
